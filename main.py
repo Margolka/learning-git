52d9e1c929e77cd6176ab150c4b8f098f78c2369
Malgorzata Sli-Ly <sliwinska.malgorzata@gmail.com>
2023-03-28 17:22:05 +0200

    New branch

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Hiszpańska inkwizycja to najlepszy sketch Monty Pythona")
-print("Ja jednak wolę "Sens życia"")
-print("A może jednak "Święty gral"?")
+print("Ja jednak wolę Sens życia")
+print("A może jednak Święty gral?")
+print("Po krótkim namyśle bardziej do mnie trafia Autostopem przez galaktykę")
